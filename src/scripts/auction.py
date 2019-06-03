0fadca8e674e5c7cbc1ff3eddc8d5f8782358e9d
Alexander Belohonov <alexander.belohonov@nixsolutions.com>
2019-06-03 19:02:06 +0300

    update auction

--- src/scripts/auction.py
+++ src/scripts/auction.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[29]:
+
 
 from web3 import Web3, HTTPProvider
 
 
-# In[ ]:
+# In[30]:
 
 
 # load abi and bytecode
@@ -16,52 +17,52 @@ with open('../contracts/auction/bytecode', 'r') as bc:
     bytecode = bc.read()
 
 
-# In[ ]:
+# In[31]:
 
 
 # web3.py instance
-w3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
+w3 = Web3(HTTPProvider('http://localhost:8545'))
 
 
-# In[ ]:
+# In[32]:
 
 
 # account data
-owner_account_address = w3.toChecksumAddress("0x0026982384f0a6c6622a01a049f9fcdcab8f0c03")
-guest_account_address = w3.toChecksumAddress("0x00221666689B516DF1f74d8E66b4F6747BB9D038")
-owner_account_password = "password"
-guest_account_password = "second_password"
+owner_account_address = w3.toChecksumAddress('0xdc88f96bcbedbd92539fefe2c91148cb67d272e4')
+guest_account_address = w3.toChecksumAddress('0x0635c434ec5f88f7dc8e44e0b878116d71d2a658')
+owner_account_password = 'password'
+guest_account_password = 'second_password'
 
 
-# In[ ]:
-
-
-# get address balance before auction
-begin_owner_balance = w3.eth.getBalance(owner_account_address)
-print(begin_owner_balance, "wei")
-print(Web3.fromWei(begin_owner_balance, "ether"), "ether")
-
-
-# In[ ]:
+# In[33]:
 
 
 # prepare contract object
 auction_deploy = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 
-# In[ ]:
+# In[34]:
+
+
+# get address balance before auction
+begin_owner_balance = w3.eth.getBalance(owner_account_address)
+print(begin_owner_balance, 'wei')
+print(Web3.fromWei(begin_owner_balance, 'ether'), 'ether')
+
+
+# In[35]:
 
 
 # unlock account before transaction
 w3.personal.unlockAccount(owner_account_address, owner_account_password)
-# Submit the transaction which deploys the contract with biddingTime 120 seconds
-tx_hash = auction_deploy.constructor(18).transact({
-    "from": owner_account_address
+# Submit the transaction which deploys the contract with biddingTime 300 seconds (5 minutes)
+tx_hash = auction_deploy.constructor(300).transact({
+    'from': owner_account_address
 })
 print(tx_hash.hex())
 
 
-# In[ ]:
+# In[36]:
 
 
 # Wait for the transaction to be mined, and get the transaction receipt
@@ -69,160 +70,183 @@ tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 print(tx_receipt)
 
 
-# In[ ]:
+# In[37]:
 
 
 # Create the contract instance with the newly-deployed address (can't use without address)
 contract = w3.eth.contract(
-    # address=tx_receipt.contractAddress,
-    address = w3.toChecksumAddress("0x61d9b0bbbe187e44493ebd6a32ff440b0b14a6a9"),
+    address = w3.toChecksumAddress(tx_receipt['contractAddress']),
     abi=abi,
 )
 
 
-# In[ ]:
+# In[38]:
 
 
 # unlock account before transaction
 w3.personal.unlockAccount(owner_account_address, owner_account_password)
 
 
-# In[ ]:
+# In[39]:
 
 
 # prepare contract function object
 bid_obj = contract.functions.bid()
+bid_gas_estimated = bid_obj.estimateGas({
+    'from': owner_account_address,
+    'value': Web3.toWei(0.03, 'ether')
+})
+print(w3.eth.gasPrice, 'current gas price in Wei, testnet')
+print(bid_gas_estimated, 'estimated for the transaction')
+transaction_cost = float(Web3.fromWei(w3.eth.gasPrice * bid_gas_estimated, 'ether')) * 132.45
+print(transaction_cost, 'payed for the transactions in USD')
 
 
-# In[ ]:
+# In[40]:
 
 
-# 1 ether 124,55$ | 5$ == 0.040 == 40 000 000 000 000 000 wei
+# 1 ether 132,45$ | 3.97$ == 0.03 == 30 000 000 000 000 000 wei
 bid_tx = bid_obj.transact({
-    "from": owner_account_address,
-    "value": Web3.toWei(0.040, 'ether'),
+    'from': owner_account_address,
+    'value': Web3.toWei(0.03, 'ether'),
+    'gas': bid_gas_estimated,
+    'gas_price': w3.eth.gasPrice
 })
 print(bid_tx.hex())
 
 
-# In[ ]:
+# In[41]:
 
 
 # Wait for the transaction to be mined, and get the transaction receipt
 print(w3.eth.waitForTransactionReceipt(bid_tx))
 
 
-# In[ ]:
+# In[42]:
 
 
 # get highestBidder using call()
-print(contract.functions.highestBidder().call(), "highestBidder")
+print(contract.functions.highestBidder().call(), 'highestBidder')
 # get highestBid
-print(contract.functions.highestBid().call(), "highestBid")
+print(contract.functions.highestBid().call(), 'highestBid')
 
 
-# In[ ]:
+# In[43]:
 
 
 # make another bid with other account, bid is less then previous
 w3.personal.unlockAccount(guest_account_address, guest_account_password)
 
 
-# In[ ]:
+# In[44]:
 
 
-# 0.03 == 30 000 000 000 000 000 wei
+# 1 ether 132,45$ | 5.30$ == 0.040 == 40 000 000 000 000 000 wei
 bid_tx = bid_obj.transact({
-    "from": guest_account_address,
-    "value": Web3.toWei(0.03, 'ether'),
+    'from': guest_account_address,
+    'value': Web3.toWei(0.040, 'ether'),
 })
 print(w3.eth.waitForTransactionReceipt(bid_tx))
 
 
-# In[ ]:
+# In[45]:
 
 
-# results shoud be the same as above
 # get highestBidder using call()
-print(contract.functions.highestBidder().call(), "highestBidder")
+print(contract.functions.highestBidder().call(), 'highestBidder')
 # get highestBid
-print(contract.functions.highestBid().call(), "highestBid")
+print(contract.functions.highestBid().call(), 'highestBid')
 
 
-# In[ ]:
+# In[46]:
+
+
+# get address balance before withdraw
+begin_owner_balance = w3.eth.getBalance(owner_account_address)
+print(begin_owner_balance, 'wei')
+print(Web3.fromWei(begin_owner_balance, 'ether'), 'ether')
+
+
+# In[47]:
 
 
 # unlock account before transaction
 w3.personal.unlockAccount(owner_account_address, owner_account_password)
 
 
-# In[ ]:
+# In[48]:
 
 
 # withdraw bid, owner
 withdraw_obj = contract.functions.withdraw()
 withdraw_tx = withdraw_obj.transact({
-    "from": owner_account_address,
+    'from': owner_account_address,
 })
 print(withdraw_tx.hex())
 
 
-# In[ ]:
+# In[49]:
 
 
 # Wait for the transaction to be mined, and get the transaction receipt
 print(w3.eth.waitForTransactionReceipt(withdraw_tx))
 
 
-# In[ ]:
+# In[50]:
 
 
-# results different - second account shoud be here
-# get highestBidder using call()
-print(contract.functions.highestBidder().call(), "highestBidder")
-# get highestBid
-print(contract.functions.highestBid().call(), "highestBid")
+# get address balance after withdraw
+begin_owner_balance = w3.eth.getBalance(owner_account_address)
+print(begin_owner_balance, 'wei')
+print(Web3.fromWei(begin_owner_balance, 'ether'), 'ether')
 
 
-# In[ ]:
+# In[55]:
 
 
 # get auctionEndTime using call()
 auction_end_time = contract.functions.auctionEndTime().call()
 print(auction_end_time)
+from datetime import datetime
 print(datetime.utcfromtimestamp(auction_end_time).strftime('%Y-%m-%d %H:%M:%S'))
 
 
-# In[ ]:
+# In[56]:
 
 
 # get beneficiary address
 print(contract.functions.beneficiary().call())
 
 
-# In[ ]:
+# In[57]:
 
 
 # unlock account to transact
 w3.personal.unlockAccount(owner_account_address, owner_account_password)
 
 
-# In[ ]:
+# In[58]:
 
 
 # finish auction if now >= auctionEndTime, transfer data of highestBid to auction owner
 auction_end_obj = contract.functions.auctionEnd()
 auction_end_tx = auction_end_obj.transact({
-    "from": owner_account_address,
+    'from': owner_account_address,
 })
 print(w3.eth.waitForTransactionReceipt(auction_end_tx.hex()))
+
+
+# In[59]:
+
+
+# get address balance after auction
+owner_balance = w3.eth.getBalance(owner_account_address)
+print(Web3.fromWei(begin_owner_balance, 'ether'), 'begin balance ether')
+print(Web3.fromWei(owner_balance, 'ether'), 'end balance ether')
 
 
 # In[ ]:
 
 
-# get address balance after auction
-owner_balance = w3.eth.getBalance(owner_account_address)
-print(Web3.fromWei(begin_owner_balance, "ether"), "begin balance 'ether'")
-print(Web3.fromWei(owner_balance, "ether"), "end balance 'ether'")
+
 
