e7a496d033306cc3600e431fc743076583b1eda9
Alexander Belohonov <alexander.belohonov@nixsolutions.com>
2019-06-17 20:56:13 +0300

    remove not needed code

--- src/actions.py
+++
@@ -1,5 +0,0 @@
-# balance = w3.eth.getBalance(w3.toChecksumAddress('0x427b2dfa3ad25e77a8b514dcdcd96da827bcb636'))
-# # 98567344999998999
-# w3.fromWei(balance, 'ether')
-# # 0.098567344999998999
-
--- src/compile_contract.py
+++
@@ -1,7 +0,0 @@
-from solc import compile_files
-
-
-# compile all contract files
-contracts = compile_files(['contract/structures.sol', 'contract/bibliography.sol'])
-
-print(contracts)
